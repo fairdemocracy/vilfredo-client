39ae7b68127bed437b52ff4500049d0379475d6c
derekpa <athens_code@gmx.com>
2015-02-19 15:20:52 +0000

    Tidy up settings file

--- static/js/settings.js
+++ static/js/settings.js
@@ -24,10 +24,10 @@ var VILFREDO_URL = 'http://0.0.0.0:8080';
 var API_VERSION = 'v2';
 
 // static files URL
-var STATIC_FILES = VILFREDO_URL + '/static'; // eg 'http://static.vilfredo.org'
+var STATIC_FILES = VILFREDO_URL + '/static';
 
 // API URL
-var VILFREDO_API_URL = VILFREDO_URL; // 'http://test.vilfredo.org'; // eg 'http://api.vilfredo.org'
+var VILFREDO_API_URL = VILFREDO_URL;
 
 // algorithm version number
 var ALGORITHM_VERSION = 2;
